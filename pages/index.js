eb6b6d89d298667b02e152090f892d028c8ab493
Adam Stox <adamstox@protonmail.com>
2022-07-23 16:32:13 -0400

    Add montserrat fontface

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,27 @@ import "normalize.css";
 import React from "react";
 import Main from "../components/Main";
 import TopBar from "../components/TopBar";
+import Head from "next/head";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-container">
-        <TopBar />
-        <Main />
+    <>
+      <Head>
+        <title>CHAIN MAIL</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <div className="App">
+        <div className="App-container">
+          <TopBar />
+          <Main />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
