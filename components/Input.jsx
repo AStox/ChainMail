737a1759c14820757f38de6054ce498584b8ae77
Adam Stox <adamstox@protonmail.com>
2022-08-28 17:48:33 -0300

    spacing changes

--- components/Input.jsx
+++ components/Input.jsx
@@ -4,18 +4,18 @@ import InsetText from "./InsetText";
 
 const Input = ({ label, labelWidth, value, setValue, placeholder, className, disabled }) => (
   <div className="Input">
-    <div className="label">
-      <InsetText height={25} width={labelWidth} textAnchor="end" text={label} />
-      <div className="underline">
-        <input
-          type="text"
-          className={`${className} input`}
-          placeholder={placeholder}
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-          disabled={disabled}
-        />
-      </div>
+    <div className="label underline">
+      <InsetText height={25} width={labelWidth} text={label} />
+      {/* <div className="underline"> */}
+      <input
+        type="text"
+        className={`${className} input`}
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        disabled={disabled}
+      />
+      {/* </div> */}
     </div>
   </div>
 );
